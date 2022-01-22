6d2667a31991e4fa1a0b1632044057a0363f84f0
Vasanth Rajendran <vaazee@gmail.com>
2022-01-22 01:53:25 -0500

    match statement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -15,4 +16,12 @@ fn main() {
         .expect("Failed to read_line");
 
     println!("Your guess: {}", guess);
+
+    let guess: u32 = guess.trim().parse().expect("Invalid input, enter a valid number.");
+
+    match guess.cmp(&secret_no) {
+        Ordering::Equal => println!("Correct, Victory!"),
+        Ordering::Less => println!("Guess is smaller, try again.."),
+        Ordering::Greater => println!("Guess is greater, try again.."),
+    }
 }
